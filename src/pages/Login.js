0ed8a37c6ed120c92197c738c50c3ce3d39beac4
bagsandk_smp <b.sandoko@sinergimp.co.id>
2022-06-02 21:21:40 +0700

    name

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -76,7 +76,7 @@ export default function Login() {
         <Container maxWidth="sm">
           <ContentStyle>
             <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
+              Sign in to Adupi Bank Sampah
             </Typography>
             <Typography sx={{ color: 'text.secondary', mb: 5 }}>Enter your details below.</Typography>
             <LoginForm />
