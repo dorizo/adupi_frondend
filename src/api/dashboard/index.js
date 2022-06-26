d3624e96c88b4af2290e3ccee5e663ae255ead17
doris hermawan <dorizo89@gmail.com>
2022-06-27 02:25:24 +0700

    maps

--- src/api/dashboard/index.js
+++ src/api/dashboard/index.js
@@ -1,5 +1,25 @@
+import qs from 'qs';
 import axios, { catchCallBack } from '../index';
 
+
+
+const GET_MAP_ANGGOTA = async (tanggalawal,tanggalakhir) => {
+  const data = qs.stringify({
+    tanggalawal,
+    tanggalakhir,
+  });
+  
+  console.log(data);
+  const headers = {
+    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+  };
+  try {
+    const response = await axios.post(`maps/pembelian`, data , { headers });
+    return response;
+  } catch (error) {
+    return catchCallBack(error);
+  }
+};
 const GET_DETAIL_TRANSAKSI = async (id) => {
   const headers = {
     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
@@ -33,4 +53,4 @@ const GET_ALL_ANGGOTA = async (id) => {
     return catchCallBack(error);
   }
 };
-export { GET_ALL_ANGGOTA, GET_ALL_ANGGOTA_BY_WILAYAH, GET_DETAIL_TRANSAKSI };
+export { GET_ALL_ANGGOTA, GET_ALL_ANGGOTA_BY_WILAYAH, GET_DETAIL_TRANSAKSI,GET_MAP_ANGGOTA };
