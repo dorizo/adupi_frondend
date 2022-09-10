7049a007ebcae856b9397bd5f0b07cf0130b7374
doris hermawan <dorizo89@gmail.com>
2022-09-10 12:09:25 +0700

    capaian data ada disini

--- src/pages/KunjunganFasilitator/index.js
+++ src/pages/KunjunganFasilitator/index.js
@@ -1,11 +1,15 @@
-import { Card, Container, Stack, TableCell, TableRow } from '@mui/material';
+import { AppBar, Button, Card, Container, Dialog, Divider, Grid, IconButton, ImageList, ImageListItem, List, ListItem, ListItemText, Modal, Stack, TableCell, TableRow, Toolbar } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { useQuery } from 'react-query';
 import { fDateTime } from '../../utils/formatTime';
 import { GET_ALL_KUNJUNGAN } from '../../api/kunjungan';
 import Page from '../../components/Page';
 import useTable from '../../hooks/useTable/index';
-
+import ReadMoreIcon from '@mui/icons-material/ReadMore';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+import { GET_viewimagekunjungan } from 'src/api/kunjunganmitra';
+import Image from 'src/components/Image';
 const headCells = [
   
   {
@@ -26,9 +30,31 @@ const headCells = [
     disablePadding: true,
     label: 'Tanggal',
   },
+  {
+    id: 'detail',
+    numeric: false,
+    disablePadding: true,
+    label: 'detail',
+  },
 ];
 
 export default function KunjunganFasilitator() {
+  const [kunjungan , Setkunjungan] = useState(null);
+  const [viewimage , Setviewimage] = useState(null);
+  const [openmodel , Setopenmodel] = useState(false);
+  const handleClose = () => {
+    Setopenmodel(false);
+  }
+
+
+  const rubahposisi = async (row) =>  {
+    console.log(row);
+    Setopenmodel(true);
+    Setkunjungan(row);
+    let details = await GET_viewimagekunjungan(row.kunjunganCode,'non mitra');
+    Setviewimage(details?.data?.data);
+  
+}
   const { data, isLoading } = useQuery('GET_ALL_KUNJUNGAN', GET_ALL_KUNJUNGAN);
 
   const rows = data && data?.data?.data;
@@ -40,8 +66,62 @@ export default function KunjunganFasilitator() {
   });
 
   return (
+    
     <Page title="Kunjungan Fasilitator">
       <Container>
+
+          <Dialog
+            fullScreen
+            open={openmodel}
+            onClose={handleClose}
+          >
+            <AppBar sx={{ position: 'relative' }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  onClick={handleClose}
+                  aria-label="close"
+                >
+                  <CloseIcon />
+                </IconButton>
+                <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                  Detail Data
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+              <List>
+              <ListItem button>
+                <ListItemText primary="Nama Fasilitator" secondary={kunjungan?.fasilitator?.nama} />
+                </ListItem>
+                <ListItem button>
+                  <ListItemText primary="Judul" secondary={kunjungan?.judul} />
+                </ListItem>
+                <Divider />
+                <ListItem button>
+                  <ListItemText primary="Judul" secondary={kunjungan?.deskripsi} />
+                </ListItem>
+              </List>
+              </Grid>
+              <Grid item xs={6}>
+                <ImageList sx={{height: 200 }} cols={3} rowHeight={164}>
+                      {viewimage?.map((li, i) => (
+                    <ImageListItem>
+                  <Image
+                          src={li?.foto}
+                          alt={`img-barang`}
+                          folder="kunjungan"
+                        />
+                    </ImageListItem>
+                    ))}
+                  </ImageList>
+              </Grid>
+            </Grid>
+            
+           
+          </Dialog>
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Kunjungan Fasilitator Non Mitra
@@ -64,6 +144,12 @@ export default function KunjunganFasilitator() {
                     </TableCell>
                     <TableCell id={labelId} scope="row">
                       {fDateTime(row.createAt)}
+                    </TableCell>
+                    <TableCell id={labelId} scope="row">
+                      <Button  onClick={() => rubahposisi(row)} >
+                        <ReadMoreIcon/>
+                      </Button>
+                      
                     </TableCell>
                   </TableRow>
                 );
--- src/pages/kunjunganmitra/index.js
+++ src/pages/kunjunganmitra/index.js
@@ -1,10 +1,16 @@
-import { Card, Container, Stack, TableCell, TableRow } from '@mui/material';
+import { AppBar, Button, Card, Container, Dialog, Divider, Grid, IconButton, ImageList, ImageListItem, List, ListItem, ListItemText, Stack, TableCell, TableRow, Toolbar } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { useQuery } from 'react-query';
 import { fDateTime } from '../../utils/formatTime';
 import { GET_ALL_KUNJUNGAN, GET_ALL_KUNJUNGANMITRA } from '../../api/kunjungan';
 import Page from '../../components/Page';
 import useTable from '../../hooks/useTable/index';
+
+import ReadMoreIcon from '@mui/icons-material/ReadMore';
+import CloseIcon from '@mui/icons-material/Close';
+import Image from 'src/components/Image';
+import { useState } from 'react';
+import { GET_viewimagekunjungan } from 'src/api/kunjunganmitra';
 
 const headCells = [
   
@@ -44,10 +50,32 @@ const headCells = [
     disablePadding: true,
     label: 'pendampingan',
   },
+  {
+    id: 'detail',
+    numeric: false,
+    disablePadding: true,
+    label: 'mode',
+  },
 ];
 
 export default function Kunjunganmitra() {
   const { data, isLoading } = useQuery('GET_ALL_KUNJUNGAN', GET_ALL_KUNJUNGANMITRA);
+  const [kunjungan , Setkunjungan] = useState(null);
+  const [viewimage , Setviewimage] = useState(null);
+  const [openmodel , Setopenmodel] = useState(false);
+  const handleClose = () => {
+    Setopenmodel(false);
+  }
+
+
+  const rubahposisi = async (row) =>  {
+    console.log(row);
+    Setopenmodel(true);
+    Setkunjungan(row);
+    let details = await GET_viewimagekunjungan(row.kunjungan_absenCode,'mitra');
+    Setviewimage(details?.data?.data);
+  
+}
 
   const rows = data && data?.data?.data;
 
@@ -62,6 +90,68 @@ export default function Kunjunganmitra() {
   return (
     <Page title="Kunjungan Fasilitator">
       <Container>
+
+      <Dialog
+            fullScreen
+            open={openmodel}
+            onClose={handleClose}
+          >
+            <AppBar sx={{ position: 'relative' }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  onClick={handleClose}
+                  aria-label="close"
+                >
+                  <CloseIcon />
+                </IconButton>
+                <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                  Detail Mitra {kunjungan?.nama}
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+              <List>
+              <ListItem button>
+                <ListItemText primary="Capaian Mitra" secondary={kunjungan?.Kunjungan_formCapaian} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Harga Pembelian" secondary={kunjungan?.Kunjungan_formHargaPembelian} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Keterlambatan" secondary={kunjungan?.Kunjungan_formKeterlambatan} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Pekerja" secondary={kunjungan?.Kunjungan_formPekerja} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="pendampingan" secondary={kunjungan?.Kunjungan_formPendampingan} />
+              </ListItem>
+            </List>
+              </Grid>
+              <Grid item xs={6}>
+              <ImageList sx={{height: 200 }} cols={3} rowHeight={164}>
+                    {viewimage?.map((li, i) => (
+                  <ImageListItem key={i}>
+                 <Image
+                        src={li?.foto}
+                        alt={`img-barang`}
+                        folder="kunjungan" 
+                      />
+                  </ImageListItem>
+                   ))}
+                 </ImageList>
+              </Grid>
+            </Grid>
+          </Dialog>
+
+
+
+
+
+
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Kunjungan Fasilitator  Mitra
@@ -93,6 +183,12 @@ export default function Kunjunganmitra() {
                     </TableCell>
                     <TableCell id={labelId} scope="row">
                       {row.Kunjungan_formPendampingan}
+                    </TableCell>
+                    <TableCell id={labelId} scope="row">
+                      <Button  onClick={() => rubahposisi(row)} >
+                        <ReadMoreIcon/>
+                      </Button>
+                      
                     </TableCell>
                     
                   </TableRow>
